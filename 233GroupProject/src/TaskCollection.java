71e8993fff9309dcb842051784cf3344483ff546
TamaraWallace <BookWyrm@10.0.0.158>
2020-02-16 12:09:47 -0700

    Loading and saving methods

--- 233GroupProject/src/TaskCollection.java
+++ 233GroupProject/src/TaskCollection.java
@@ -6,14 +6,6 @@ import java.util.ArrayList; // import the ArrayList class
 import java.util.Date; // import date utility, copied from Task class
 import java.util.stream.Stream;
 
-/* Team Questions: (Also posted on Slack)
- * -need to be able to get user id to create new task
- * -how do we want the tasks to be displayed
- * -how do we want the tasks to be stored in the csv file
- * -when saving, I was thinking it would be best to only save/append new tasks, does this sound 
- *  reasonable? if so, need a way to track which tasks are new
- *  -do we want tasks to be sorted in the task collection and if so by what value
-*/
 public class TaskCollection {
 
 	private ArrayList<Task> tasks = new ArrayList<Task>(); // An ArrayList of all the user's tasks.
@@ -21,7 +13,7 @@ public class TaskCollection {
 	// Method Name: loadUsrTasks
 	// Parameters: fname (the name of the file to load tasks from) and userID (the id of the user whose tasks are being loaded)
 	// Return: a TaskCollection object
-	// Functionality: load a file containing all tasks, determine the value of the nextKey, store user's tasks in an ArrayList
+	// Functionality: load a file containing all tasks, save all tasks to current arraylist of tasks, save users tasks to new collection and return object
 	public static TaskCollection loadUsrTasks(String fname, int userID) {
 		TaskCollection usersTasks = new TaskCollection();
 		// based on method 3 from this website:https://examples.javacodegeeks.com/core-java/java-8-read-file-line-line-example/ is used for basic file reading
@@ -31,15 +23,12 @@ public class TaskCollection {
 		            String[] taskList = lines.toArray(String[] :: new);
 		            lines.close();
 		            br.close();
-		            //
 		            for(String task : taskList) {
-		            	//System.out.println(task.getClass().getSimpleName());
-		            	// change to add all tasks and then remove ones with invalid user id
 		            	String[] splitTask = task.split(",");
+		            	String[] d = splitTask[splitTask.length-1].split("-");
+	            		Date due = new Date(Integer.parseInt(d[0].trim()), Integer.parseInt(d[1].trim())-1,Integer.parseInt(d[2].trim()));
+	            		Task next = new Task(Integer.parseInt(splitTask[1].trim()),splitTask[2], splitTask[3],Boolean.parseBoolean(splitTask[4].trim()),due);
 		            	if(splitTask[1].trim().equals(String.valueOf(userID))) {
-		            		String[] d = splitTask[splitTask.length-1].split("-");
-		            		Date due = new Date(Integer.parseInt(d[0].trim()), Integer.parseInt(d[1].trim())-1,Integer.parseInt(d[2].trim()));
-		            		Task next = new Task(Integer.parseInt(splitTask[1].trim()),splitTask[2], splitTask[3],Boolean.parseBoolean(splitTask[4].trim()),due);
 		            		usersTasks.tasks.add(next);
 		            	}
 		            }
@@ -52,32 +41,52 @@ public class TaskCollection {
 	// Method Name: saveTasks
 	// Parameters: fname (the name of the file to save tasks to)
 	// Return: void
-	// Functionality: ?
-	// NOTE: I know it needs to save the tasks, but I'm not sure what the best implementation is
+	// Functionality: save all the users tasks to the file, including new tasks and updates
 	public void saveTasks(String fname) {
+		System.out.println("Saving. Please do not close the program.");
 		try {
-			FileWriter bw = new FileWriter(fname, true);
 			BufferedReader br = new BufferedReader(new FileReader(fname));
             Stream <String> lines = br.lines();
             String[] taskList = lines.toArray(String[] :: new);
             lines.close();
             br.close();
-            Task.setNextID(taskList.length);
-            //change to if line key is in tasks array update line
-            for(Task task: this.tasks) {
-            	if(task.getNextID()<task.getTaskID()) {
-            		Date d = task.getDueDate();
-            		bw.write(String.valueOf(task.getTaskID())+", "+String.valueOf(task.getUserID())+", "+task.getName()+", "+task.getNotes()+", "+String.valueOf(task.isCompleted())+", "+String.valueOf(d.getYear())+"-"+String.valueOf(d.getMonth()+1)+"-"+String.valueOf(d.getDate())+"\n");
+            FileWriter bw = new FileWriter(fname);
+            int lastKey = taskList.length - 1;
+            ArrayList<String> tList = new ArrayList<String>();
+            for(String s: taskList) {
+            	for(Task task: this.tasks) {
+            		if(Character.getNumericValue(s.charAt(0))==task.getTaskID()) {
+            			Date d = task.getDueDate();
+            			s = String.valueOf(task.getTaskID())+", "+String.valueOf(task.getUserID())+", "
+            			+task.getName()+", "+task.getNotes()+", "+String.valueOf(task.isCompleted())+", "
+            			+String.valueOf(d.getYear())+"-"+String.valueOf(d.getMonth()+1)+"-"+String.valueOf(d.getDate());
+            		}
+            	}
+            	tList.add(s+"\n");
+            }
+            
+            for(Task t : this.tasks) {
+            	if (lastKey<t.getTaskID()) {
+            		Date d = t.getDueDate();
+            		tList.add(String.valueOf(t.getTaskID())+", "+String.valueOf(t.getUserID())+", "
+                			+t.getName()+", "+t.getNotes()+", "+String.valueOf(t.isCompleted())+", "
+                			+String.valueOf(d.getYear())+"-"+String.valueOf(d.getMonth()+1)+"-"+String.valueOf(d.getDate())+"\n");
             	}
             }
+            
+            for(String str : tList) {
+            	bw.write(str);
+            }
+            
             bw.close();
+            System.out.println("Saving Complete.\nIt is now safe to close the program.");
 		} catch (IOException io) {
 			
 		}
 		
 		
 	}
-	
+
 	// Method Name: display
 	// Parameters: none
 	// Return: void
@@ -88,6 +97,7 @@ public class TaskCollection {
 			System.out.println("***********************");
 			System.out.println(t.toString());
 		}
+		//NOTE: Keeping old display method just in case.
 		/*for(int i = 0; i<this.tasks.size(); i++) {
 			System.out.println(String.valueOf(i+1)+".  "+tasks.get(i).getName()+"\t"+String.valueOf(tasks.get(i).getDueDate().getYear())+"-"+String.valueOf(tasks.get(i).getDueDate().getMonth()+1)+"-"+String.valueOf(tasks.get(i).getDueDate().getDate()));
 			System.out.println("\t\t"+tasks.get(i).getNotes());
@@ -101,12 +111,11 @@ public class TaskCollection {
 	// Parameters: key (key of the task to retrieve)
 	// Return: the task requested
 	// Functionality: given a task key, return a task from a task collection
-	// NOTE: Possibly use a more efficient search method? Also, need some way to get the userID
 	public Task getTask(int key) {
 		Task currentTask = null; //temporarily assigning null to the object to be returned
-		for(int i = 0; i < tasks.size(); i++) {
-			if (tasks.get(i).getTaskID() == key) {
-				currentTask = tasks.get(i);
+		for(Task t : tasks) {
+			if (t.getTaskID() == key) {
+				currentTask = new Task(t);
 			}
 		}
 		return currentTask;
@@ -116,7 +125,6 @@ public class TaskCollection {
 	// Parameters: name, notes and date (all the respective fields for a task)
 	// Return: void
 	// Functionality: creates a new task and adds it to the tasks collection
-	// NOTE: Although not mentioned in the UML, it needs to be passed the userID as well
 	public void addTask(Task t) {
 		tasks.add(t);
 	}
@@ -136,15 +144,15 @@ public class TaskCollection {
 	}
 	
 	/*public ArrayList<Task> getTasks() {
-	TaskCollection temp = new TaskCollection();
-	for(Task t : this.tasks) {
-		temp.addTask(new Task(t));
+		TaskCollection temp = new TaskCollection();
+		for(Task t : this.tasks) {
+			temp.addTask(new Task(t));
+		}
+		return temp.tasks;
 	}
-	return temp.tasks;
-}
 
-public void setTasks(ArrayList<Task> tasks) {
-	this.tasks = tasks;
-}*/
+	public void setTasks(ArrayList<Task> tasks) {
+		this.tasks = tasks;
+	}*/
 	
 }

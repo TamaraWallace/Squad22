0d09e51fd660d3d0c2ceeb28c6f090c326f31b36
steph <steph@DESKTOP-HGFV906>
2020-02-16 15:08:58 -0700

    Menu Navigation is functional

--- 233GroupProject/src/TaskCollection.java
+++ 233GroupProject/src/TaskCollection.java
@@ -25,36 +25,35 @@ public class TaskCollection {
 	// Method Name: loadUsrTasks
 	// Parameters: fname (the name of the file to load tasks from) and userID (the id of the user whose tasks are being loaded)
 	// Return: a TaskCollection object
-	// Functionality: load a file containing all tasks, determine the value of the nextKey, store user's tasks in an ArrayList
-	public static TaskCollection loadTasks(String fname) {
+	// Functionality: load a file containing all tasks, save all tasks to current arraylist of tasks, save users tasks to new collection and return object
+	public static TaskCollection loadUsrTasks(String fname, int userID) {
 		TaskCollection usersTasks = new TaskCollection();
 		// based on method 3 from this website:https://examples.javacodegeeks.com/core-java/java-8-read-file-line-line-example/ is used for basic file reading
-				try {
-		            BufferedReader br = new BufferedReader(new FileReader(fname));
-		            Stream <String> lines = br.lines();
-		            String[] taskList = lines.toArray(String[] :: new);
-		            lines.close();
-		            br.close();
-		            //
-		            for(String task : taskList) {
-		            	//System.out.println(task.getClass().getSimpleName());
-		            	String[] splitTask = task.split(",");
-	            		String[] d = splitTask[splitTask.length-1].split("-");
-	            		Date due = new Date(Integer.parseInt(d[0].trim()), Integer.parseInt(d[1].trim())-1,Integer.parseInt(d[2].trim()));
-	            		Task next = new Task(Integer.parseInt(splitTask[1].trim()),splitTask[2], splitTask[3],Boolean.parseBoolean(splitTask[4].trim()),due);
-	            		usersTasks.tasks.add(next);
-	            	}
-		        } catch (IOException io) {
-		            io.printStackTrace();
-		        }
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(fname));
+			Stream <String> lines = br.lines();
+			String[] taskList = lines.toArray(String[] :: new);
+			lines.close();
+			br.close();
+			for(String task : taskList) {
+				String[] splitTask = task.split(",");
+				String[] d = splitTask[splitTask.length-1].split("-");
+				Date due = new Date(Integer.parseInt(d[0].trim()), Integer.parseInt(d[1].trim())-1,Integer.parseInt(d[2].trim()));
+				Task next = new Task(Integer.parseInt(splitTask[1].trim()),splitTask[2], splitTask[3],Boolean.parseBoolean(splitTask[4].trim()),due);
+	            if (splitTask[1].trim().equals(String.valueOf(userID))) {
+	            	usersTasks.tasks.add(next);
+	            }
+			}
+		} catch (IOException io) {
+			io.printStackTrace();
+		}
 		return usersTasks;
 	}
 	
 	// Method Name: saveTasks
 	// Parameters: fname (the name of the file to save tasks to)
 	// Return: void
-	// Functionality: ?
-	// NOTE: I know it needs to save the tasks, but I'm not sure what the best implementation is
+	// Functionality: save all the users tasks to the file, including new tasks and updates
 	public void saveTasks(String fname) {
 		System.out.println("Saving. Please do not close the program.");
 		try {
@@ -103,30 +102,21 @@ public class TaskCollection {
 	// Return: void
 	// Functionality: display all tasks in the current task collection
 	public void display( ) {
-
 		for (Task t: tasks) {
-			System.out.println("***********************");
+			System.out.println("--------------------------------");
 			System.out.println(t.toString());
 		}
-		/*for(int i = 0; i<this.tasks.size(); i++) {
-			System.out.println(String.valueOf(i+1)+".  "+tasks.get(i).getName()+"\t"+String.valueOf(tasks.get(i).getDueDate().getYear())+"-"+String.valueOf(tasks.get(i).getDueDate().getMonth()+1)+"-"+String.valueOf(tasks.get(i).getDueDate().getDate()));
-			System.out.println("\t\t"+tasks.get(i).getNotes());
-			if(!(tasks.get(i).getNotes().equals(""))) {
-				System.out.println("");
-			}
-		}*/
 	}
 	
 	// Method Name: getTask
 	// Parameters: key (key of the task to retrieve)
 	// Return: the task requested
 	// Functionality: given a task key, return a task from a task collection
-	// NOTE: Possibly use a more efficient search method? Also, need some way to get the userID
 	public Task getTask(int key) {
-		Task currentTask = null; //temporarily assigning null to the object to be returned
-		for(int i = 0; i < tasks.size(); i++) {
-			if (tasks.get(i).getTaskID() == key) {
-				currentTask = tasks.get(i);
+		Task currentTask = null;
+		for (Task t: tasks) {
+			if (t.getTaskID() == key) {
+				currentTask = new Task(t);
 			}
 		}
 		return currentTask;
@@ -136,7 +126,6 @@ public class TaskCollection {
 	// Parameters: name, notes and date (all the respective fields for a task)
 	// Return: void
 	// Functionality: creates a new task and adds it to the tasks collection
-	// NOTE: Although not mentioned in the UML, it needs to be passed the userID as well
 	public void addTask(Task t) {
 		tasks.add(t);
 	}

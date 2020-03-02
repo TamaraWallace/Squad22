14e24d2f4837a700cf84d04fd919303e47d95d9f
senoucimeriam <senoucimeriam@localhost>
2020-03-01 17:13:22 -0700

    Add Task GUI

---
+++ 233GroupProject/src/Application/AddTaskController.java
@@ -0,0 +1,76 @@
+package Application;
+
+import java.net.URL;
+import java.time.LocalDate;
+import java.util.ResourceBundle;
+import java.util.UUID;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class AddTaskController  implements Initializable{
+	
+		@FXML 
+		private TextField name,notes;
+		@FXML
+		private DatePicker date;
+		
+		@FXML 
+		private Label dateLabel,nameLabel;
+		
+		public void addButton() {
+			
+			try {
+			// get strings
+			String taskName = name.getText();
+			String taskNotes = notes.getText();
+			
+			// validating taskName is not empty
+			if (taskName.isBlank()) {
+				nameLabel.setText("TASK NAME CAN'T BE EMPTY");
+				taskName = name.getText();
+				
+			} else {
+				nameLabel.setText("");
+			}
+			
+			// validating the taskDate is not empty 
+			LocalDate taskDate = date.getValue();
+			if (taskDate == null) {
+				dateLabel.setText("TASK DATE CAN'T BE EMPTY");
+			} else {
+				dateLabel.setText("");
+				System.out.println(taskDate);
+			}
+			
+			
+			// if everything is good, make a task: 
+			// UUID userID = user.getUsrID();
+			// Task task = new Task(userID, taskName, taskNotes, false, taskDate);
+			// add to some array list??? 
+	
+			System.out.println(taskName);
+			System.out.println(taskNotes);
+			
+			
+			
+			
+			}
+			catch(Exception e) {
+				System.out.println(e);
+			}
+		}
+		@Override
+		public void initialize(URL arg0, ResourceBundle arg1) {
+
+			// TODO Auto-generated method stub
+			
+	
+		}
+		public void backButton() {
+			// to do when main menu is made 
+		}
+}

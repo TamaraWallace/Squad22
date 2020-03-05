7b0bf69d78eb8eff8fb3a3d1953a1fa4b340e6bb
PS1718 <sahotap1@hotmail.com>
2020-03-04 22:59:41 -0700

    HomeScreen GUI complete, Controller Class Partial

--- 233GroupProject/src/Application/HomeScreenController.java
+++ 233GroupProject/src/Application/HomeScreenController.java
@@ -1,6 +1,70 @@
-package application;
+package Application;
+
+import java.net.URL;
+
+import javafx.fxml.FXML;
 
 public class HomeScreenController {
+	
+	@FXML
+	private RadioButton mostRecentTask;
+	
+	@FXML
+	private RadioButton secondRecentTask;
+	
+	@FXML
+	private RadioButton thirdRecentTask;
+	
+	@FXML
+	private Button viewAll;
+	
+	@FXML
+	private Button profileButton;
+	
+	@FXML
+	private Button addTask;
+	
+	@FXML 
+	private Label helloUser, currentTasksTitle, viewAllLabel;
+	
+	public HomeScreenController() {
+		
+	}
+	
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+
+		// TODO Auto-generated method stub
+	}
+	
+	
+	public void mostRecentTask() {
+		
+	}
+	
+	public void secondRecentTask() {
+		
+	}
+	
+	public void thirdRecentTask() {
+		
+	}
+	
+	//do we need another scene for view all or direct to Task Menu? 
+	public void viewAll() {
+		
+	}
+	
+	public void addTask() {
+		
+	}
+	
+	
+	//can add later for demo 3 
+	public void profileButton() {
+		
+	}
+
 	
 	
 	

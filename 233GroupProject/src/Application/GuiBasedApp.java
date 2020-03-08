2568886b86b65bc2dc449074b9d581b542104487
TamaraWallace <60551202+TamaraWallace@users.noreply.github.com>
2020-03-08 16:16:44 -0600

    Changed main(String args) to main(String[] args)

--- 233GroupProject/src/Application/GuiBasedApp.java
+++ 233GroupProject/src/Application/GuiBasedApp.java
@@ -45,7 +45,7 @@ public class GuiBasedApp extends Application{
 		GuiBasedApp.user = user;
 	}
 	
-	public static void main(String args) {
+	public static void main(String[] args) {
 		launch(args);
 	}
 	@Override

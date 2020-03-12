973b457faf7c69a6f0bcad5792d59e0ad02b6b6d
senoucimeriam <senoucimeriam@192.168.0.7>
2020-03-11 22:28:53 -0600

    EditTaskController Variables Declared

--- 233GroupProject/src/Application/EditTaskController.java
+++ 233GroupProject/src/Application/EditTaskController.java
@@ -1,5 +1,24 @@
 package Application;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class EditTaskController {
 
+
+	@FXML 
+	private TextField name,notes;
+	@FXML
+	private DatePicker date;
+	
+	@FXML 
+	private Label dateLabel,nameLabel;
+	
+	@FXML
+	private Button backBtn;
+	
+	
 }

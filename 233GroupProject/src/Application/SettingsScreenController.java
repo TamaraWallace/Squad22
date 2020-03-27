7f47a08f76c9a40367c38b3b14928aad1de0838d
senoucimeriam <senoucimeriam@localhost>
2020-03-26 19:02:49 -0600

    Variables for textfields & buttons

--- 233GroupProject/src/Application/SettingsScreenController.java
+++ 233GroupProject/src/Application/SettingsScreenController.java
@@ -1,5 +1,15 @@
 package Application;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
 public class SettingsScreenController {
 
+	@FXML 
+	private TextField userName,email; //associated with text object for task name & notes in FXML
+	
+	@FXML
+	private Button backBtn; //associated with the back button object in FXML
+	private Button updateBtn; // associated with the update button object in FXML
 }
